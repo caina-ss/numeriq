a20c761b0bba8f431c92e03f68394ee66a80ab02
Cainã Souza <caina.souza@alayacare.com>
2020-11-10 18:56:26 -0500

    Initial Commit

---
+++ NumeriQ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NumeriQ
+//
+//  Created by Caina Souza on 2020-11-10.
+//  Copyright © 2020 Quebecor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
