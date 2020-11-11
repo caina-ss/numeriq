86ccb4c0aa329b4c253b4c25587fe36fb386c127
Cainã Souza <caina.souza@alayacare.com>
2020-11-10 23:44:26 -0500

    Implements logic for collection view

--- NumeriQ/Scenes/Home/HomeViewController.swift
+++ NumeriQ/Scenes/Home/HomeViewController.swift
@@ -7,13 +7,68 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import SwinjectStoryboard
 
 class HomeViewController: UIViewController {
 
+    // MARK: - Outlets
+
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    // MARK: - Dependencies
+
+    var viewModel: HomeViewModelType?
+
+    // MARK: - Private
+
+    private let disposeBag = DisposeBag()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        configureCollectionView()
+        viewModel?.fetchArticles(query: "bitcoin", from: Date())
     }
 
 }
 
+// MARK: - Private
+
+private extension HomeViewController {
+
+    func configureCollectionView() {
+        collectionView.dataSource = self
+        collectionView.register(ArticleCell.self)
+
+        viewModel?
+            .articles
+            .subscribe(onNext: { _ in
+                self.collectionView.reloadData()
+            })
+            .disposed(by: disposeBag)
+    }
+
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        viewModel?.articles.value.count ?? 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ArticleCell.defaultReuseIdentifier,
+                                                      for: indexPath) as! ArticleCell
+
+        guard let article = viewModel?.articles.value[indexPath.row] else {
+            return .init()
+        }
+
+        cell.configure(article: article)
+
+        return cell
+    }
+
+}
